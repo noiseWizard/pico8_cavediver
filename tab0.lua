dd343355cb6ee886dd43bc5dff15930141b942a1
noiseWizard <[64124242+noiseWizard@users.noreply.github.com]>
2025-07-06 20:14:46 +1000

    added restart game feature

--- tab0.lua
+++ tab0.lua
@@ -9,6 +9,8 @@ function _update()
         update_cave()
         move_player()
         check_hit()
+    else
+        if (btnp(5)) _init() --restart
     end
 end
 
@@ -18,8 +20,9 @@ function _draw()
     draw_player()
 
     if (game_over) then
-        print("game over!", 44, 44, 7)
-        print("your score:"..player.score,34,54,7)
+        print("game over!",44, 44, 7)
+        print("your score:"..player.score, 34, 54, 7)
+        print("press ❎ to play again!", 18, 72, 6)
     else
         print("score:"..player.score,2,2,7)
     end
